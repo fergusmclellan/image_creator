a348b8d19246f64a501c26b2a2e62bedb9c292b7
Fergus McLellan <33413126+fergusmclellan@users.noreply.github.com>
2020-09-11 22:02:49 +0100

    Update test.py
    Updated test.py to use code_text as the json key.

--- test.py
+++ test.py
@@ -4,16 +4,15 @@ from image_creation_app import app
 my_multiline_input = f'''
 import requests
 mystring = requests.get(url, header, data)
-this is being processed by Docker version
+this is being processed by Jenkins pipeline
 '''
-image_filename = "item_1111111.png"
 
 class TestHello(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
     def test_hello(self):
-        response = self.app.post('/api/add_message/1234', json={"mytext": my_multiline_input})
+        response = self.app.post('/api/add_message/1234', json={"code_text": my_multiline_input})
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(response.mimetype, 'image/png')
         print('Test confirmed that image received!')
