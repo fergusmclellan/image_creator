d086961ce284a9d050ea9ffd77f7d38f76a1f0af
Fergus McLellan <33413126+fergusmclellan@users.noreply.github.com>
2020-09-11 18:25:07 +0100

    Update test.py
    Updated to POST text and check for image/png as mimetype

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import unittest
 from image_creation_app import app
+
+my_multiline_input = f'''
+import requests
+mystring = requests.get(url, header, data)
+this is being processed by Docker version
+'''
+image_filename = "item_1111111.png"
+
 class TestHello(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
     def test_hello(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        response = self.app.post('/api/add_message/1234', json={"mytext": my_multiline_input}))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.mimetype, 'image/png')
 if __name__ == '__main__':
     unittest.main()
