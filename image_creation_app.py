75a9abcf4812581103b33d5146f661b741599f74
Fergus McLellan <33413126+fergusmclellan@users.noreply.github.com>
2020-09-12 15:39:06 +0100

    Update image_creation_app.py
    Fixed bugs, and moved CodeText class out into a file

--- image_creation_app.py
+++ image_creation_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, send_file
 from PIL import Image, ImageDraw, ImageFont
+from codetext import CodeText
 
 FONT_HEIGHT_SIZE_PX = 15
 FONT_WIDTH_SIZE_PX = 9
@@ -11,16 +12,6 @@ IMAGE_FONT = ImageFont.truetype('cour.ttf', FONT_HEIGHT_SIZE_PX)
 
 
 app = Flask(__name__)
-
-@app.route('/api/create_image', methods=['POST'])
-class CodeText:
-    def __init__(self, text, width, lines):
-        self.text = text
-        self.width = width
-        self.lines = lines
-        self.width_pixels = 0
-        self.height_pixels = 0
-        self.state = "OK"
 
 def find_lines_and_width(text):
     max_width = 0
@@ -36,16 +27,17 @@ def find_lines_and_width(text):
         no_of_lines = 1
     return max_width, no_of_lines
 
-def add_message():
+@app.route('/api/create_image/<uuid>', methods=['GET', 'POST'])
+def create_image(uuid):
     content = request.json
     max_width, no_of_lines = find_lines_and_width(content['code_text'])
-    print("max width: " + str(max_width))
-    print("max lines: " + str(no_of_lines))
+    #print("max width: " + str(max_width))
+    #print("max lines: " + str(no_of_lines))
     my_code = CodeText(content['code_text'], max_width, no_of_lines)
     image_filename = 'output.png'
 
-    my_code.width_pixels = (self.width * FONT_WIDTH_SIZE_PX) + (BORDER_PADDING_PX * 2)
-    my_code.height_pixels = ((self.lines - 1) * (FONT_HEIGHT_SIZE_PX + LINE_SPACING_PX)) + FONT_HEIGHT_SIZE_PX + (BORDER_PADDING_PX * 2)
+    my_code.width_pixels = (my_code.width * FONT_WIDTH_SIZE_PX) + (BORDER_PADDING_PX * 2)
+    my_code.height_pixels = ((my_code.lines - 1) * (FONT_HEIGHT_SIZE_PX + LINE_SPACING_PX)) + FONT_HEIGHT_SIZE_PX + (BORDER_PADDING_PX * 2)
 
     if my_code.width_pixels > MAX_WIDTH_PX:
         my_code.state = "TOO WIDE!"
@@ -63,7 +55,7 @@ def add_message():
     drawing.text((BORDER_PADDING_PX, BORDER_PADDING_PX), my_code.text, font=IMAGE_FONT, fill=('black'))
     drawing.rectangle([(0,0), (my_code.width_pixels, my_code.height_pixels)], fill=None, outline='black',width=2)
     # border appears as only 1 pixel width along right and bottom sides, so draw an extra line
-    drawing.line((1, my_code.width_pixels-2, my_code.width_pixels-1, my_code.height_pixels-2), width=1, fill='black')
+    drawing.line((1, my_code.height_pixels-2, my_code.width_pixels-1, my_code.height_pixels-2), width=1, fill='black')
     drawing.line((my_code.width_pixels-2, 1, my_code.width_pixels-2, my_code.width_pixels-1), width=1, fill='black')
     img.save(image_filename)
 
